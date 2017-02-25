d3f8c75712caed35e2f30ea7771143bbc0c19d74
spaceghost28 <spaceghost28@gmail.com>
2017-02-25 10:32:34 -0500

    Add form on index and post route for project

---
+++ app/models/project.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var timeEntrySchema = new Schema({
+	_userId: Schema.Types.ObjectId,
+	startTime: Date,
+	endTime: Date
+});
+
+var storyCardSchema = new Schema({
+	cardNumber: Number,
+	timeEntries: [timeEntrySchema]
+});
+
+var projectSchema = new Schema({
+	projectName: String,
+	client: String,
+	storyCards: [storyCardSchema]
+});
+
+var Project = mongoose.model('Project', projectSchema);
+
+module.exports = Project;
--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,26 @@
+var Project = require('./models/project');
+
 module.exports = function(app) {
 
-  app.post('/tryme', function(req, res) {
-    res.json({
-      "message": "yo dawg, this worked"
+    app.get('/', function(request, response) {
+        response.render('index.ejs');
     });
-  });
+
+    app.post('/project', function(request, response) {
+        Project.findOne({projectName: request.body.projectName }, function(err, project) {
+            if (err) console.log('error finding project');
+            else if (!project) {
+                console.log('no project found');
+            }
+            else {
+                console.log('we found a project');
+            }
+            response.redirect('/');
+        });
+    });
+
+    app.post('/time', function(request, response) {
+
+    });
 
 };
